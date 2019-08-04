95fb1f87a803cf2c8bed721753b2a08f9bfd1ba7
rhawiz <rawandhawiz@gmail.com>
2019-08-04 14:36:16 +0200

    IQLink.exe

--- iqfeed_startup.sh
+++ iqfeed_startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-iqconnect_exe="/root/.wine/drive_c/Program Files/DTN/IQFeed/iqconnect.exe"
+iqconnect_exe="/root/.wine/drive_c/Program Files/DTN/IQFeed/iqlink.exe"
 if [ -f "$iqconnect_exe" ]
 then
 	echo "$iqconnect_exe found. launching iqfeed client"
-  /usr/bin/wine /root/.wine/drive_c/Program\ Files/DTN/IQFeed/iqconnect.exe -autoconnect
+  /usr/bin/wine /root/.wine/drive_c/Program\ Files/DTN/IQFeed/iqlink.exe -autoconnect
 else
 	echo "$iqconnect_exe not found. launcing iqfeed client installer"
   /usr/bin/wine /root/iqfeed_client_5_2_5_0.exe
