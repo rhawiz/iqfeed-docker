979181ccb5840d4877b5b9e18bd407fca1dce296
rhawiz <rawandhawiz@gmail.com>
2019-08-07 13:05:46 +0200

    Upgraded iqfeed client, launch iqlink.exe instead

--- iqfeed_startup.sh
+++ iqfeed_startup.sh
@@ -6,5 +6,5 @@ then
   /usr/bin/wine /root/.wine/drive_c/Program\ Files/DTN/IQFeed/iqlink.exe -autoconnect
 else
 	echo "$iqconnect_exe not found. launcing iqfeed client installer"
-  /usr/bin/wine /root/iqfeed_client_5_2_5_0.exe
+  /usr/bin/wine /root/iqfeed_client_6_0_1_1.exe
 fi
